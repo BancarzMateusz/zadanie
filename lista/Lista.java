e62f4d5e4ac0cba0ba843b742a7d8c7f76a107f0
student <student@stanowisko.sala308>
2026-04-10 14:11:12 +0200

    Zadanie

--- lista/Lista.java
+++ lista/Lista.java
@@ -1,15 +1,15 @@
 import java.util.ArrayList;
 public class Lista {
  public static void main(String[] args) {
- ArrayList<String> produkty = new ArrayList<>();
- produkty.add(new Lista("Japko", 3, 5));
- produkty.add(new Lista("Pomarancz", 4,6));
- produkty.add(new Lista("Terracota pie", 1 ,2));
+ ArrayList<Produkt> produkty = new ArrayList<Produkt>();
+ produkty.add(new Produkt("Japko", 3, 5));
+ produkty.add(new Produkt("Pomarancz", 4,6));
+ produkty.add(new Produkt("Terracota pie", 1 ,2));
 
 
  System.out.println("Lista produktow: " + produkty);
-	for(int x: produkty){
-	System.out.println(x);
+	for(Produkt produkt : produkty){
+	System.out.println(produkt);
 }
  }
 }
